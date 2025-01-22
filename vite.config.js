c410c66084fa7393babb83319ac2aef6bda0582a
A-3-huaman25 <huamanpintokevin@fpllefia.com>
2025-01-22 19:39:28 +0100

    Projecto configurado

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
- root: path.resolve(__dirname, 'src'),
- publicDir: path.resolve(__dirname, 'public'),
+    root: path.resolve(__dirname, 'src'), // Define 'src' como raíz
+    publicDir: path.resolve(__dirname, 'public'), // Carpeta para recursos estáticos
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, 'src'),
+            '@': path.resolve(__dirname, 'src'), // Alias para importaciones
         },
+    },
+    build: {
+        outDir: path.resolve(__dirname, 'dist'), // Generar la salida en 'dist' (en la raíz)
+        emptyOutDir: true, // Limpia la carpeta 'dist' antes de construir
     },
 });
